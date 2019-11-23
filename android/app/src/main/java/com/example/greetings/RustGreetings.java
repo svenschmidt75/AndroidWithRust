d19a7a119f6b411fc0d727326c124b4fddd278f1
Sven <svenschmidt75@googlemail.com>
2019-11-23 11:18:15 -0700

    initial commit

---
+++ android/app/src/main/java/com/example/greetings/RustGreetings.java
@@ -0,0 +1,10 @@
+package com.example.greetings;
+
+public class RustGreetings {
+
+    private static native String greeting(final String pattern);
+
+    public String sayHello(String to) {
+        return greeting(to);
+    }
+}
